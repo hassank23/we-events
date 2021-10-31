c6b296e8be378761a58c420dd2d657323bd7bdcb
hassan katbay <hassan.katbay@net.usj.edu.lb>
2021-10-31 22:35:52 +0200

    menu bar

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -27,8 +27,8 @@ class Header extends Component{
                 <div id="topbar" className="d-flex align-items-center fixed-top">
                     <div className="container d-flex justify-content-center justify-content-md-between">
                       <div className="contact-info d-flex align-items-center">
-                        <i className="bi bi-phone d-flex align-items-center"><span>+961 01 736 333</span></i>
-                        <i className="bi bi-clock d-flex align-items-center ms-4"><span> Mon-Sat: 8AM - 5PM</span></i>
+                        <i className="fa fa-phone d-flex align-items-center"><span>+961 01 736 333</span></i>
+                        <i className="fa fa-clock-o d-flex align-items-center ms-4"><span> Mon-Sat: 8AM - 5PM</span></i>
                       </div>
                     </div>
                   </div>
