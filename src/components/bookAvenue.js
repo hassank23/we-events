b77a4712e978963953350f1cd12fac9c2231e8d2
hassank23 <85111182+hassank23@users.noreply.github.com>
2022-01-10 11:38:31 +0200

    Update bookAvenue.js

--- src/components/bookAvenue.js
+++ src/components/bookAvenue.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
-import { Form, Label, FormGroup, Input, Col, FormFeedback } from 'reactstrap';
+import { Form, Label, FormGroup, Input, Col, FormFeedback ,Row} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Menu from "./menu";
 class Avenue extends Component {  
+  constructor(props){
+    super(props);
+this.state = {
+    Avenue:'',
+    
+    
+};
+this.handleInputChange = this.handleInputChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
+
+}
+
+handleInputChange(event) {
+const target = event.target;
+const value = target.type === 'checkbox' ? target.checked : target.value;
+const name = target.name;
+
+this.setState({
+  [name]: value
+});
+}
+
+handleSubmit(event) {
+console.log('Current State is: ' + JSON.stringify(this.state)+"submeting complete for your wedding");
+alert('Current State is: ' + JSON.stringify(this.state));
+event.preventDefault();
+}
    
 render(){
 
 
-  const submitForm = async () => {
-    console.log("Submitting complete");
-  };
+  
   
     return(<div className="col-12" >
         
@@ -20,17 +45,31 @@ render(){
             </div>
             <div className="row">
                <Menu/>
-             
-                          <div >
+               </div>
+               <Form onSubmit={this.handleSubmit}>
+             <div className="row px-5">
+              
+             <label className="input-group-text col-2" for="inputGroupSelect01">Avenue</label>
+                                <select  className="col-8" id="inputGroupSelect01" name="avenue" value={this.state.avenue}
+                                        onChange={this.handleInputChange} >
+                                    <option value="Life_venue">Life Venue</option>
+                                    <option value="Hilton">Hilton</option>
+                                    <option value="Pheonicia">Pheonicia</option>
+                                    <option value="season">Four Season hotel</option>
+                                  
+                                </select>
+                                      
+             </div>
+                          <div>
                                     <Link type="submit" to="/home"className="btn mb-5 center col-5 ">
                                        submit
                                     </Link>
                             </div>
-                               
+                            </Form>             
             </div>
                
-              
-           </div>
+            
+           
     )}
 }
 
