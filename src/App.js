0b5a3be28cf8147de568052ab8d3127d808f09c6
hassank23 <85111182+hassank23@users.noreply.github.com>
2022-01-10 11:32:52 +0200

    aditing app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React ,{ Component } from 'react';
 import './App.css';
-import {Provider} from "react-redux";
+//import {Provider} from "react-redux";
 import Main from "./components/mainComponents";
+import { BrowserRouter} from 'react-router-dom';
+//import {ConfigureStore} from './redux/configureStore';
+//const store=ConfigureStore();
 import {BrowserRouter as Router} from 'react-router-dom';
-import {ConfigureStore} from './redux/configureStore';
-const store=ConfigureStore();
+//import {ConfigureStore} from './redux/configureStore';
+//const store=ConfigureStore();
+
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+
+      //<Provider store={store}>
+      //<Provider>
+      <BrowserRouter>
+      <div className="App">
+        <Main/>
+      </div>
+      </BrowserRouter>
+      //</Provider>
+
+   /*   <Provider store={store}>
       <Router>
       <div className="App">
         <Main/>
       </div>
       </Router>
       </Provider>
-    );
+
+    );*/);
   }
 }
 export default App;
