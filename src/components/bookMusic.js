7d733e103d677b3903838cd5173de963eef8c09c
hassank23 <85111182+hassank23@users.noreply.github.com>
2022-01-10 11:37:32 +0200

    Update bookMusic.js

--- src/components/bookMusic.js
+++ src/components/bookMusic.js
@@ -1,87 +1,98 @@
 import React, { Component } from "react";
-import { Col, Row } from 'reactstrap';
+import { Col, FormGroup, Row,Form,Input} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LocalForm ,Control} from "react-redux-form";
+
 
 class Music extends Component {
     constructor(props){
         super(props);
        
-        
-        
+        this.state = {
+            music: '',
+            singer: '',
+            request_music: '',
+            
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
+        
     }
+        
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
     
-    
-   handleSubmit(event) {
-       console.log('Current State is: ' + JSON.stringify(this.state));
-       alert('Current State is: ' + JSON.stringify(this.state));
-       event.preventDefault();
-   }
-   
-       
+        this.setState({
+          [name]: value
+        });
+    }
 
-           
-       
-   
-       
+    handleSubmit(event) {
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }       
 render(){
-  
     return(<div className="py-5 row-content" id='music-bg'>
                <div className="col-12 py-3 row center">
                      <h3  className="pt-5" style={{color:"yellow"}}>book your music</h3>
                </div>
 
                <div className="col-12 ">
-                        <LocalForm onSubmit={this.handleSubmit}>  
-                        <Row className="form-group">
+                        <Form onSubmit={this.handleSubmit}>  
+                        <FormGroup row >
                                 <div className=" row  px-5  ">
                                 <Col className="col-lg-6 col-md-6 col-sm-6 col-6">
                                 <div className="input-group mb-3 cp">
                                 <label class="input-group-text" for="inputGroupSelect04">music</label>
-                                <Control.select model=".music" className="form-control" id="inputGroupSelect04" name="music"  >
+                                <select  className="col-lg-10" id="inputGroupSelect04" name="music" value={this.state.music}
+                                        onChange={this.handleInputChange} >
                                     <option value="yes">yes</option>
                                     <option value="no">no</option>
-                                </Control.select>
+                                </select>
                                 </div>
                                 </Col>
                                 
                                 <Col className="col-lg-6 col-md-6 col-sm-6 col-6">
                                 <div className="input-group mb-3 cp">
                                 <label class="input-group-text" for="inputGroupSelect02">singer</label>
-                                <Control.select model=".singer" className="form-control" id="inputGroupSelect02" name="singer">
+                                <select className="col-lg-10"  id="inputGroupSelect02" name="singer"value={this.state.singer}
+                                        onChange={this.handleInputChange}>
                                     <option value="yes">yes</option>
                                     <option value="no">no</option>
-                                </Control.select>
+                                </select>
                                 </div>
                                 </Col>   
                                 </div>
-                            </Row>
+                            </FormGroup>
 
-                            <Row className="form-group" >
+                            <FormGroup row >
                                 <div className="row px-5 ">
                                 <Col  className="col-lg-12 col-md-12 col-sm-12">
                                 <div className="input-group">
                                 <span className="input-group-text">special request</span>
                                 
-                                <Control.text model=".request" className="form-control"id="request" name="request"  aria-label="special request" 
+                                <Input id="request_music" name="request_music"  aria-label="special request" 
                                         rows="6" placeholder="special request here"
+                                         
+                                        value={this.state.request_music}
+                                        onChange={this.handleInputChange}
                                         />
                                 </div>
                                 </Col>
                                 </div>
-                                </Row>
+                                </FormGroup>
 
-                            <Row className="form-group">
+                            <FormGroup row>
                                 <div className="row pt-3 "> 
                                 <Col className="col-lg-12 col-md-12 col-sm-12 col-12">
-                                <Link to="/book_photography" className="btnn col-12 " >next</Link>
+                                <Link to="/book_wedding/book_photography" className="btnn col-12 " >next</Link>
                                 </Col>
                                 </div>
-                            </Row>
+                            </FormGroup>
                                
-                            </LocalForm></div>
+                            </Form></div>
 
 
         
