e87777a571b48394992bdb6c6ed4fd8d5e6c3ad7
hassank23 <85111182+hassank23@users.noreply.github.com>
2022-01-10 11:39:14 +0200

    Update book_photography.js

--- src/components/book_photography.js
+++ src/components/book_photography.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
-import {Label, Col, Row} from 'reactstrap';
+import {Label, Col, Row, Input,Radio} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm,Control } from "react-redux-form";
 
 class Photography extends Component {
     constructor(props){
         super(props);
+    this.state = {
+        location:'',
+        numberOfPhotos:'',
+        datePhoto:'',
+        agree:'',
+        request_photo:'',
         
-        this.handleSubmit = this.handleSubmit.bind(this);
-        
-    }
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     
+}
     
-   handleSubmit(event) {
-       console.log('Current State is: ' + JSON.stringify(this.state));
-       alert('Current State is: ' + JSON.stringify(this.state));
-       event.preventDefault();
-   }     
+handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+}
+
+handleSubmit(event) {
+    console.log('Current State is: ' + JSON.stringify(this.state));
+    alert('Current State is: ' + JSON.stringify(this.state));
+    event.preventDefault();
+}
 render(){
   
     return(<div className="py-5 row-content" id='photo-bg'>
@@ -32,8 +49,9 @@ render(){
                                 <div className="input-group">
                                 <span className="input-group-text ">location</span>
                                 
-                                <Control.text model=".location" id="location" name="location" className="form-control " aria-label="location" 
-                                        rows="1" placeholder="put the location that you need"
+                                <Input id="location" name="location" className="form-control " aria-label="location" 
+                                        rows="1" placeholder="put the location that you need" value={this.state.location}
+                                        onChange={this.handleInputChange}
                                         />
                                 </div>
                                 </Col>
@@ -48,8 +66,9 @@ render(){
                                 <div className="input-group">
                                 <span className="input-group-text">number Of Photos</span>
                                 
-                                <Control type="number" model=".number" id="numberOfPhotos" name="numberOfPhotos" className="form-control" aria-label="number Of Photos" 
-                                        rows="1" placeholder="put the number Of Photos you need"
+                                <Input type="number"  id="numberOfPhotos" name="numberOfPhotos" className="form-control" aria-label="number Of Photos" 
+                                        rows="1" placeholder="put the number Of Photos you need"value={this.state.numberOfPhotos}
+                                        onChange={this.handleInputChange}
                                         />
                                 </div>
                                 </Col>
@@ -62,8 +81,9 @@ render(){
                            <div className="row container px-5 pt-2">
                            <Col  className="col-lg-6 col-md-6 col-sm-6  ">
                            <span className="input-group-text  ">put the date of photography session down</span>
-                                    <Control type="date" model=".datePhoto"  id="datePhoto" name="datePhoto"
-                                        className="mt-2 "
+                                    <Input type="date" model=".datePhoto"  id="datePhoto" name="datePhoto" value={this.state.datePhoto}
+                                        onChange={this.handleInputChange}
+                                     className="mt-2 "
                                        />
                                    
                                          
@@ -76,8 +96,10 @@ render(){
                                     <Row className="form-group"radio>
                                     <span className="input-group-text px-2 ">may we publish your photo-session?</span>
                                         <Label radio>
-                                            < Control.radio model=".agree"
+                                            <Input type='radio' model=".agree"
                                                 name="agree"
+                                                value={this.state.agree}
+                                        onChange={this.handleInputChange}
                                                 /> {'yes'}
                                                 
                                         </Label>
@@ -97,8 +119,9 @@ render(){
                                 <div className="input-group">
                                 <span className="input-group-text ">special request</span>
                                 
-                                <Control.text model=".request" id="request" name="request" className="form-control" aria-label="special request" 
-                                        rows="2" placeholder="special request here"
+                                <Input model="request_photo" id="request_photo" name="request_photo" className="form-control" aria-label="special request"
+                                        rows="2" placeholder="special request here" value={this.state.request_photo}
+                                        onChange={this.handleInputChange}
                                         
                                         />
                                 </div>
@@ -109,7 +132,7 @@ render(){
                                 <Row className="form-group">
                                 <div className="row pt-3 "> 
                                 <Col className="col-lg-6 col-md-6 col-sm-6 col-6">
-                                <Link to="/book_Avenue" className="btnn col-12 " >next</Link>
+                                <Link to="/book_wedding/book_Avenue" className="btnn col-12 " >next</Link>
                                 </Col>
                                 </div>
                             </Row>
